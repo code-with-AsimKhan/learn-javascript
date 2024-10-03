ad8883b4a650e73b71dcbdd7d9af28dc0876fa4d
Asim khan <asimkhank9837@gmail.com>
2024-10-03 16:41:55 +0000

    learn variables

---
+++ 01_basics/variable.js
@@ -0,0 +1,14 @@
+const accountId = 1454455
+let accountEmail = "asimkhan898@gmail.com"
+var accountPassword = "123455"
+
+acountCity = "agra"
+
+//  accountId = 3   //not allowed change value in const 
+accountEmail = "asimk@gmail.com";
+accountPassword = "5566";
+acountCity = "amroha"
+ //console.log(accountId);
+
+ console.table([accountId, acountCity, accountEmail, accountPassword])
+ 
