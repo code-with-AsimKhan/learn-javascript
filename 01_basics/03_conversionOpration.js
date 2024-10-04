1b3bb7080c2ffca9913cde1e990b5c3b3de15f0e
Asim khan <asimkhank9837@gmail.com>
2024-10-04 13:05:44 +0000

    operations

--- 01_basics/03_conversionOpration.js
+++ 01_basics/03_conversionOpration.js
@@ -1,10 +1,10 @@
 let score = "33abn"
-console.log(typeof score);
+//console.log(typeof score);
 
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33
@@ -14,8 +14,53 @@ console.log(valueInNumber);
 let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1 => true ; 0 => false ;
 // " " => false;
 // "Asim" => true;
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ********************************* Operations ***************************************
+
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Asim";
+let str2 = " Khan"
+let str3 = str1 + str2;
+//console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(2 + 2 + "1");
+
+// console.log((2 + 3 )* 2 / 4);
+
+
+
+
+
+
+
+
+
