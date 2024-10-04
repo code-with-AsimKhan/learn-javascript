ea49e2a4945206d42c565efcaed93a0e51441e79
Asim khan <asimkhank9837@gmail.com>
2024-10-04 10:42:32 +0000

    learn dataTypes

---
+++ 01_basics/dataType.js
@@ -0,0 +1,25 @@
+"use strict";  // Treat all js code as newer version.
+
+// alert(3 + 3)  // We are using node.js, not browser.
+
+let  name  = "Asim Khan"
+let age = 18;
+let isLoggedIn = true;
+let states;
+
+// number => 2 to 53 something 
+// BigInt 
+// string => ""
+// Boolean => true/false;
+// null => standalone value
+// undefined => abhi vlue assign nahi hui he
+// synbol => unique
+
+
+// object
+
+console.log(typeof age);
+console.log(typeof undefined);  //undefined
+console.log( typeof null);      //object
+
+
