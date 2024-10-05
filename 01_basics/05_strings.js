2d6dc2407a978d0e974849ea518bca0e6fb8d03f
Asim khan <asimkhank9837@gmail.com>
2024-10-05 10:46:25 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,20 @@
+const name = "AsimKhan";
+const repoCount = 20;
+
+// console.log(name + repoCount + "Value"); // don't use like this. 
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);  // Ise stringInterPolation kha jata h.
+
+const gameName = new String("khan")
+
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toLowerCase());
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2)); // element position ke liye.
+console.log(gameName.indexOf('a')); // ye bhi element ki position, isme  element lete h.
+
+
+
+
+
