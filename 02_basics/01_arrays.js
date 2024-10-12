d7f1a3a8c0d1a2f42d1bb27dd51c2a22da8ad0e1
Asim khan <asimkhank9837@gmail.com>
2024-10-12 10:46:29 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,11 @@
+// Arrays
+
+const myArr = [0, 1, 2, 3, 4 ,5];
+const myfruits = ["mango", "banana", "apple"];
+const myArr2 = new Array(1, 2, 3, 4, 5);
+
+console.log(myArr[2]);
+
+// ********************************* Arrey methods ****************************************
+
+
