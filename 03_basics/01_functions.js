c205ee7410a6f1163d547b33862c9921e40b35e0
Asim khan <asimkhank9837@gmail.com>
2024-10-18 10:47:16 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,17 @@
+function sayMyName(){
+    console.log("A");
+    console.log("s");
+    console.log("i");
+    console.log("m");
+    
+}
+
+//sayMyName(); // sayMyName ho gaya refrence, () ye hogaya execution.
+
+
+function addTwoNumber(number1, number2){
+    console.log(number1 + number2);
+    
+}  
+
+addTwoNumber(3, 6);
