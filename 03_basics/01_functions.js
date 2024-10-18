92360a54d39992c8ef2b1b07d3a2aff644841105
Asim khan <asimkhank9837@gmail.com>
2024-10-18 16:30:11 +0000

    learn functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -9,9 +9,70 @@ function sayMyName(){
 //sayMyName(); // sayMyName ho gaya refrence, () ye hogaya execution.
 
 
-function addTwoNumber(number1, number2){
-    console.log(number1 + number2);
+// function addTwoNumber(number1, number2){
+//     console.log(number1 + number2);
     
-}  
+// }  
 
-addTwoNumber(3, 6);
+// addTwoNumber(3, 6);
+
+function addTwoNumber(number1, number2){
+   // let result = number1 + number2;
+   // return result;
+      return  number1 + number2;
+}
+
+const result = addTwoNumber(3,5)
+
+//console.log("Result: ", result);
+
+
+
+
+function loginUserMessage(username){
+   // if(username === undefined){ 
+      if(!username){          // iska doosra tarika ye h.
+        console.log("please enter a username");
+        return;
+        
+    }
+    return `${username} just logged in`;
+}
+
+//console.log(loginUserMessage("AsimKhan"));
+//console.log(loginUserMessage());
+
+
+
+function calculateCartPrice(...num1){ // spread/Rest: ye value add karke array me deta h.
+    return num1;
+}
+
+//console.log(calculateCartPrice(200, 550, 400));
+
+
+const user = {
+    userName: "AsimKhan",
+    price: 99
+}
+
+function handleObjects(anyobject){
+    console.log(`userName is ${anyobject.userName} and price is ${anyobject.price}`);
+    
+}
+//handleObjects(user);  // isme object pass karna padega.
+handleObjects({         // isme object direct bhi desakte h.
+    userName: "AsimKhan",
+    price: 99
+})
+
+
+
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondArray(getArray){
+    return getArray[2];
+}
+
+//console.log(returnSecondArray(myNewArray));
+console.log(returnSecondArray( [200, 400, 100, 600])); // isme direct Array bhi de sakte h.
