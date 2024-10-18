635f32b7733427466d584b500330b00b44a187df
Asim khan <asimkhank9837@gmail.com>
2024-10-18 10:28:27 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -29,13 +29,42 @@ const obj2 = {3: "c", 4: "d"}
 //const obj3 = Object.assign({}, obj1, obj2) // object.assign: ye object ko add karta h, but ise km use karte h.
 
 const obj3 = {...obj1, ...obj2} // Spread: ye bhi object ko add karta h, but ise jiyada use karte h.
-console.log(obj3);
+// console.log(obj3);
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));  // object.keys: isme sari keys deta h, array ki form me output milta h.
-console.log(Object.values(tinderUser)); // object.values: isme sari values deta h, array ki form me output milta h.
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));  // object.keys: isme sari keys deta h, array ki form me output milta h.
+// console.log(Object.values(tinderUser)); // object.values: isme sari values deta h, array ki form me output milta h.
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogged')); // hasOwnProperty: ye object me property check karne ke liye.
+// console.log(tinderUser.hasOwnProperty('isLogged')); // hasOwnProperty: ye object me property check karne ke liye.
 
+// object deStructuring.
+
+const course = {
+    courseName: "Js in Hindi", 
+    price: "999",
+    courseIntructor: "AsimKhan"
+}
+
+ //console.log(course.courseIntructor);
+
+ const {courseIntructor: instructor, price} = course // destructuring: isme name bhi change kar sakte h,
+ console.log(instructor);                            // aur key ke name bhi change karsakte h.
+
+ console.log(price);
+ 
+
+//  {                        // Api jo h object ki form me bhi miljati h.
+//     "name": "asimKhan",
+//     "courseName": "js in Hindi",
+//     "price": "999"
+//  }
+ 
+ 
+// [         // Api jo h Array ki form me bhi miljati h.
+//     {},
+//     {},
+//     {}
+// ]
+  
