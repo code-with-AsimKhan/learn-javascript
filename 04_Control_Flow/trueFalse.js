f3030b07987fc37a165961ae0d20d7eedcca1383
Asim khan <asimkhank9837@gmail.com>
2024-10-20 13:03:02 +0000

    truthy Values

--- 04_Control_Flow/trueFalse.js
+++ 04_Control_Flow/trueFalse.js
@@ -25,6 +25,33 @@ if(userEmail){
 const emptyObj = {}
 
 if(Object.keys(emptyObj).length){
-    console.log("object is Empty");
+    //console.log("object is Empty");
     
 }
+
+
+// false == 0;  // ye true dega, bowser me check karsakte he.
+// false == ""; // ye true dega, bowser me check karsakte he.
+// 0 == "";     // ye true dega, bowser me check karsakte he.
+
+
+//  Nullish Coalescing Operator (??); // ye bus indono me use hota h, 1: NuLL, 2: undefined.
+
+let val1;
+//val1 = 5 ?? 10;  // output: 5
+//val1 = null ?? 10;  // output: 10
+//val1 = undefined ?? 15  // output: 15
+//val1 = null ?? 10 ?? 20 // output: 10
+//val1 = null ?? undefined  // output: undefined
+
+console.log(val1);
+
+
+//    Terniary Operater
+
+//condition ? true : false
+
+const teaPrice = 100;
+teaPrice >= 80 ? console.log("less than 80") : console.log("more than 80");
+
+
